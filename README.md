9df0001e365cf38fb13d54d53e193734f8adcda4
ciitraa <ciitrardhanares@gmail.com>
2025-03-10 10:28:13 +0700

    update 10.28

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-Nama    : Citra Ayu Ardhanareswari
-NIM     : 1313623006
-Prodi   : Ilmu Komputer 2023 A
+# Identity :
+- Name      : Citra Ayu Ardhanareswari
+- NIM       : 1313623006
+- Github    : https://github.com/ciitraa
 
+# Instruction :
+1. Open terminal 
+2. Type 'g++ src/main.cpp -o maxheap' in terminal
+3. Then type './maxheap'
--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,19 @@ void maxHeapify(int arr[], int n, int i) {
     int left = 2 * i + 1;  // Left child
     int right = 2 * i + 2; // Right child
 
-    // If left child is larger than root
     if (left < n && arr[left] > arr[largest])
         largest = left;
 
-    // If right child is larger than largest so far
     if (right < n && arr[right] > arr[largest])
         largest = right;
 
-    // If largest is not root
     if (largest != i) {
         swap(arr[i], arr[largest]);
-        // Recursively heapify the affected sub-tree
         maxHeapify(arr, n, largest);
     }
 }
 
-// Function to build a Max-Heap from the given array
+// Function to build a max heap
 void buildMaxHeap(int arr[], int n) {
     // Index of last non-leaf node
     for (int i = n / 2 - 1; i >= 0; i--) {
@@ -40,6 +36,9 @@ void printArray(int arr[], int n) {
 int main() {
     int arr[] = {16, 4, 10, 14, 7, 9, 3, 2, 8, 1};
     int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Heap: ";
+    printArray(arr, n);
 
     buildMaxHeap(arr, n);
 
